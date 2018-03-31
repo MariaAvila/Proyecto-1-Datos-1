2e2d9abec08a03f91e0b18822e812c539efc1f8c
Maria Avila <31579784+MariaAvila@users.noreply.github.com>
2018-03-31 17:39:22 -0600

    Version 0.1
    Se logra imprimir al jugador en pantalla

---
+++ Proyecto 1/src/application/GameObject.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+
+public class GameObject {
+	private Node view;
+    private Point2D velocity = new Point2D(0, 0);
+
+    private boolean alive = true;
+
+    public void update() {
+        view.setTranslateX(view.getTranslateX() + velocity.getX());
+        view.setTranslateY(view.getTranslateY() + velocity.getY());
+    }
+    
+    public GameObject(Node view) {
+        this.view = view;
+    }
+
+    public void setVelocity(Point2D velocity) {
+        this.velocity = velocity;
+    }
+
+    public Point2D getVelocity() {
+        return velocity;
+    }
+
+    public Node getView() {
+        return view;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean isDead() {
+        return !alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public boolean isColliding(GameObject other) {
+        return getView().getBoundsInParent().intersects(other.getView().getBoundsInParent());
+    }
+}
---
+++ Proyecto 1/src/application/Player.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Player extends GameObject {
+
+	public Player() {
+		super(new Rectangle(40, 20, Color.BLUE));
+	}
+
+}
