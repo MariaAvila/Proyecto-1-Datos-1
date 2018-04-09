f4bf6faeed270637526386b96e8749f3bfc76f72
Maria Avila <31579784+MariaAvila@users.noreply.github.com>
2018-04-09 16:05:17 -0600

    Version 0.2
    Se puede mover el personaje en la pantalla

--- Proyecto 1/src/application/Player.java
+++ Proyecto 1/src/application/Player.java
@@ -1,13 +1,28 @@
 package application;
 
 import javafx.scene.Node;
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Player extends GameObject {
 
 	public Player() {
-		super(new Rectangle(40, 20, Color.BLUE));
+		super(new Rectangle(20, 20, Color.BLUE));
+	}
+	
+	void moveX(int value) {
+		if (value< 0) {
+			if(getView().getTranslateX()== 0) {
+				return;
+			}
+		}
+		else {
+			if(getView().getTranslateX()+20== 600) {
+				return;
+			}
+		}
+		getView().setTranslateX(getView().getTranslateX() + value);
 	}
 
 }
